b7c3ce586869f33c611a88d18700ee267ef675b2
sina <sinaosooli1384@gmail.com>
2023-12-05 14:30:29 +0330

    Sina added a new function

--- calculator.c
+++ calculator.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int calculate(int a, int b){
   
-<<<<<<< HEAD
+
   // A combination of both calculations
   return a * b * 2 -10 + (a + b);
+}
+int pow2(int a){
+
+	return a * a;
 }
